60e2b437719b1c0972159506c959ac312de20f46
FransiskusAriyanto <fransiskusariyanto2400@gmail.com>
2024-04-02 09:48:26 +0700

    fix: fix data stored

--- src/components/fragments/Form/FormForgotPassword.jsx
+++ src/components/fragments/Form/FormForgotPassword.jsx
@@ -1,8 +1,8 @@
-import { useForm } from 'react-hook-form';
-import Label from '../../elements/Label';
-import { ArrowLeftIcon } from '@heroicons/react/20/solid';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useForm } from "react-hook-form";
+import Label from "../../elements/Label";
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 //#region latency waiting for transfer data to server and get email feedback from server--manual delay for test
 
@@ -23,7 +23,7 @@ const FormForgotPassword = () => {
         formState: { errors },
         reset,
     } = useForm({
-        mode: 'onTouched',
+        mode: "onTouched",
     });
     //#endregion
     //#region latency alert
@@ -32,7 +32,7 @@ const FormForgotPassword = () => {
             console.log(data);
             setButtonDisabled(true);
             await latency(3000);
-            alert('Please check your email for the next steps');
+            alert("Please check your email for the next steps");
             reset();
             setButtonDisabled(false);
         } catch (error) {
@@ -43,39 +43,39 @@ const FormForgotPassword = () => {
     return (
         <>
             <form
-                className="space-y-6"
-                action="#"
-                method="POST"
+                className='space-y-6'
+                action='#'
+                method='POST'
                 onSubmit={handleSubmit(onSubmit)}
             >
                 <div>
                     <Label
-                        htmlFor="email"
+                        htmlFor='email'
                         validation={
-                            errors.mail ? 'text-red-500' : 'text-gray-900'
+                            errors.email ? "text-red-500" : "text-gray-900"
                         }
                     >
-                        Email <span className="text-[#E02222]">*</span>
+                        Email <span className='text-[#E02222]'>*</span>
                     </Label>
-                    <div className="mt-2">
+                    <div className='mt-2'>
                         <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            autoComplete="email"
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3 ring-1"
-                            {...register('mail', {
-                                required: 'Email required',
+                            id='email'
+                            name='email'
+                            type='email'
+                            autoComplete='email'
+                            className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3 ring-1'
+                            {...register("mail", {
+                                required: "Email required",
                                 pattern: {
                                     value: /\S+@\S+\.\S+/,
                                     message:
-                                        'Invalid email format. ex: frato@ms.mii.co.id',
+                                        "Invalid email format. ex: frato@ms.mii.co.id",
                                 },
                             })}
                         />
-                        {errors.mail && (
-                            <p className="text-sm text-[#E02222]">
-                                {errors.mail.message}
+                        {errors.email && (
+                            <p className='text-sm text-[#E02222]'>
+                                {errors.email.message}
                             </p>
                         )}
                     </div>
@@ -83,21 +83,21 @@ const FormForgotPassword = () => {
 
                 <div>
                     <button
-                        type="submit"
+                        type='submit'
                         onClick={() => handleSubmit(onSubmit)}
-                        className="flex justify-center w-full h-full rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-[#5c6ac4]"
+                        className='flex justify-center w-full h-full rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-[#5c6ac4]'
                         disabled={buttonDisabled}
                     >
                         Submit
                     </button>
                 </div>
             </form>
-            <p className="mt-6 text-sm text-center text-gray-500 float-end">
+            <p className='mt-6 text-sm text-center text-gray-500 float-end'>
                 <Link
-                    to={'/login'}
-                    className="font-bold leading-6 text-indigo-600 hover:text-indigo-500 flex gap-x-2 items-center"
+                    to={"/login"}
+                    className='font-bold leading-6 text-indigo-600 hover:text-indigo-500 flex gap-x-2 items-center'
                 >
-                    <ArrowLeftIcon className="h-5 w-5" />
+                    <ArrowLeftIcon className='h-5 w-5' />
                     Login
                 </Link>
             </p>
--- src/components/fragments/Form/FormLogin.jsx
+++ src/components/fragments/Form/FormLogin.jsx
@@ -1,8 +1,8 @@
-import { useForm } from 'react-hook-form';
-import { Button, Label } from '../../elements';
-import { useState } from 'react';
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/20/solid';
-import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { Button, Label } from "../../elements";
+import { useState } from "react";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/20/solid";
+import { Link, useNavigate } from "react-router-dom";
 
 const FormLogin = () => {
     //#region Ari test
@@ -18,105 +18,105 @@ const FormLogin = () => {
     const navigate = useNavigate();
 
     const onSubmit = (data) => {
-        localStorage.setItem('email', data.mail);
-        localStorage.setItem('password', data.password);
-        navigate('/dashboard');
+        localStorage.setItem("email", data.email);
+        localStorage.setItem("password", data.password);
+        navigate("/dashboard");
         reset();
     };
     //#endregion
     return (
         <>
             <form
-                className="space-y-6"
-                action="#"
-                method="POST"
+                className='space-y-6'
+                action='#'
+                method='POST'
                 onSubmit={handleSubmit(onSubmit)}
             >
-                <div className="flex flex-col">
+                <div className='flex flex-col'>
                     <Label
-                        htmlFor="email"
+                        htmlFor='email'
                         validation={
-                            errors.mail ? 'text-red-500' : 'text-gray-900'
+                            errors.email ? "text-red-500" : "text-gray-900"
                         }
                     >
-                        Email <span className="text-[#E02222]">*</span>
+                        Email <span className='text-[#E02222]'>*</span>
                     </Label>
-                    <div className="mt-2">
+                    <div className='mt-2'>
                         <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            autoComplete="email"
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3 ring-1"
-                            {...register('mail', {
-                                required: 'Email required',
+                            id='email'
+                            name='email'
+                            type='email'
+                            autoComplete='email'
+                            className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3 ring-1'
+                            {...register("email", {
+                                required: "Email required",
                                 pattern: {
                                     value: /\S+@\S+\.\S+/,
                                     message:
-                                        'Invalid email format. ex: frato@ms.mii.co.id',
+                                        "Invalid email format. ex: frato@ms.mii.co.id",
                                 },
                             })}
                         />
-                        {errors.mail && (
-                            <p className="text-sm text-[#E02222]">
-                                {errors.mail.message}
+                        {errors.email && (
+                            <p className='text-sm text-[#E02222]'>
+                                {errors.email.message}
                             </p>
                         )}
                     </div>
                 </div>
 
                 <div>
-                    <div className="flex items-center justify-between">
+                    <div className='flex items-center justify-between'>
                         <Label
-                            htmlFor="password"
+                            htmlFor='password'
                             validation={
                                 errors.password
-                                    ? 'text-red-500'
-                                    : 'text-gray-900'
+                                    ? "text-red-500"
+                                    : "text-gray-900"
                             }
                         >
-                            Password <span className="text-[#E02222]">*</span>
+                            Password <span className='text-[#E02222]'>*</span>
                         </Label>
-                        <div className="text-sm">
+                        <div className='text-sm'>
                             <Link
-                                to={'/forgot-password'}
-                                className="font-bold text-blue-600"
+                                to={"/forgot-password"}
+                                className='font-bold text-blue-600'
                             >
                                 forgot password?
                             </Link>
                         </div>
                     </div>
-                    <div className="mt-2 relative w-full">
+                    <div className='mt-2 relative w-full'>
                         <input
-                            id="password"
-                            name="password"
-                            type={showPassword ? 'text' : 'password'}
-                            autoComplete="current-password"
+                            id='password'
+                            name='password'
+                            type={showPassword ? "text" : "password"}
+                            autoComplete='current-password'
                             className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset 
                             focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3 ring-1`}
-                            {...register('password', {
-                                required: 'Password required',
+                            {...register("password", {
+                                required: "Password required",
                                 minLength: {
                                     value: 8,
                                     message:
-                                        'Password minimum 8 characters. Please try again.',
+                                        "Password minimum 8 characters. Please try again.",
                                 },
                             })}
                         />
                         <button
-                            type="button"
+                            type='button'
                             onClick={() => setShowPassword(!showPassword)}
-                            className="absolute inset-y-1 right-2 top-1 flex items-center"
+                            className='absolute inset-y-1 right-2 top-1 flex items-center'
                         >
                             {showPassword ? (
-                                <EyeSlashIcon className="h-5 w-5 text-gray-700 items-center" />
+                                <EyeSlashIcon className='h-5 w-5 text-gray-700 items-center' />
                             ) : (
-                                <EyeIcon className="h-5 w-5 text-gray-700 items-center" />
+                                <EyeIcon className='h-5 w-5 text-gray-700 items-center' />
                             )}
                         </button>
                     </div>
                     {errors.password && (
-                        <p className="text-sm text-[#E02222]">
+                        <p className='text-sm text-[#E02222]'>
                             {errors.password.message}
                         </p>
                     )}
@@ -124,8 +124,8 @@ const FormLogin = () => {
 
                 <div>
                     <Button
-                        type="submit"
-                        size="size-96"
+                        type='submit'
+                        size='size-96'
                         onClick={() => handleSubmit(onSubmit)}
                     >
                         Login
