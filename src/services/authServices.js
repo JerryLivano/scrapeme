406c0873cb1651b370c984851700f96aaf45289a
FransiskusAriyanto <fransiskusariyanto2400@gmail.com>
2024-04-04 13:51:26 +0700

    service

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,19 +1,10 @@
-import api from "../app/api/apiSlice";
+import { BaseApiAccount } from "../assets/api/Api";
 import { jwtDecode } from "jwt-decode";
 
-/**
- *
- * @param {*} token
- */
-const setToken = (token, refreshToken) => {
+const setToken = (token) => {
     localStorage.setItem("token", token);
-    localStorage.setItem("refreshToken", refreshToken);
 };
 
-/**
- *
- * @returns
- */
 const getToken = () => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -22,41 +13,46 @@ const getToken = () => {
     return null;
 };
 
-const login = async (userdata) => {
-    try {
-        const responseToken = await api.post("login", userdata);
-        console.log(responseToken?.data?.access_token);
-        const token = responseToken?.data?.access_token;
-        const refreshToken = responseToken?.data?.refresh_token;
-
-        const headers = {
-            Authorization: `Bearer ${token}`,
-        };
-        setToken(token, refreshToken);
-
-        const responseData = await api.get("profile", { headers });
-        console.log("ini admin", responseData?.data?.role);
-
-        localStorage.setItem("email", userdata.email);
-        localStorage.setItem("password", userdata.password);
-        localStorage.setItem("name", responseData?.data?.name);
-        localStorage.setItem("role", responseData?.data?.role);
-        return responseData;
-    } catch (error) {
-        console.log(error);
-    }
+const login = (userdata) => {
+    const response = BaseApiAccount.post("login", userdata);
+    localStorage.setItem("email", userdata.email);
+    localStorage.setItem("password", userdata.password);
+    return BaseApiAccount.post("login", userdata);
 };
 
 const getUserRole = () => {
-    return localStorage.getItem("role");
+    const token = getToken();
+    const roleType =
+        "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+    if (token) {
+        const payload = jwtDecode(token);
+        console.log(payload[roleType]);
+        isLoggedIn();
+        return payload[roleType];
+    }
+    return null;
 };
 
 const getUserEmail = () => {
-    return localStorage.getItem("email");
+    const token = getToken();
+    const emailType =
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+    if (token) {
+        const payload = jwtDecode(token);
+        return payload[emailType];
+    }
+    return null;
 };
 
 const getUserName = () => {
-    return localStorage.getItem("name");
+    const token = getToken();
+    const username =
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
+    if (token) {
+        const payload = jwtDecode(token);
+        return payload[username];
+    }
+    return null;
 };
 
 const isLoggedIn = () => {
@@ -76,8 +72,14 @@ const isLoggedIn = () => {
 
 const refreshToken = async () => {
     try {
-        const refreshToken = localStorage.getItem("refreshToken");
-        return setToken(refreshToken);
+        const email = localStorage.getItem("email");
+        const password = localStorage.getItem("password");
+        const userdata = { email, password };
+        const response = await login(userdata);
+        console.log(response.data);
+        console.log(response.data.data);
+        setToken(response.data.data);
+        return true;
     } catch (error) {
         console.log("error", error);
         return false;
