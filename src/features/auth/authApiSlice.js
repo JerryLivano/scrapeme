d054615b5f28ee2b91354cdbd87b0f629a9c4309
JerryLivano <2172002@maranatha.ac.id>
2024-05-13 13:03:50 +0700

    fix: login endpoint

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -7,7 +7,7 @@ export const authApiSlice = apiSlice.injectEndpoints({
         //add another query
         login: builder.mutation({
             query: (credentials) => ({
-                url: '/login',
+                url: 'api/v1/login',
                 method: 'POST',
                 body: { ...credentials}
             }),
