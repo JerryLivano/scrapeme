a1589cce67c2b0b9b1f84037f68ff349d23fdb6f
JerryLivano <2172002@maranatha.ac.id>
2024-05-27 17:54:09 +0700

    fix: login loading

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -9,6 +9,7 @@ import { setCredentials } from "../../features/auth/authSlice";
 import Toast from "../../components/elements/NotificationProvider/Index";
 import { setAuthToken } from "../../utils/authUtilities";
 import ButtonLogin from "../../components/elements/Button/ButtonLogin";
+import Spinner from "../../components/elements/Spinner/Spinner";
 
 const Login = () => {
     const {
@@ -21,7 +22,7 @@ const Login = () => {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const [login] = useLoginMutation();
+    const [login, { isLoading }] = useLoginMutation();
 
     const onSubmit = async (data) => {
         const userData = { email: data.email, password: data.password };
@@ -45,53 +46,61 @@ const Login = () => {
 
     return (
         <>
-            <Toast toastType={toastType} setTypeToast={setToastType} />
-            <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
-                <div className='flex flex-col'>
-                    <Label htmlFor='email' name='Email address' />
-                    <div>
-                        <InputGroup
-                            id='email'
-                            type='email'
-                            name='email'
-                            placeholder='Email address'
-                            errors={errors}
-                            required
-                            register={register}
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div className='flex items-center justify-between'>
-                        <Label htmlFor='password' name='Password' />
-                        <div className='text-sm'>
-                            <a
-                                href='/password/forgot'
-                                className='font-semibold text-indigo-600 hover:text-indigo-500'
-                            >
-                                Forgot password?
-                            </a>
+            {isLoading && <Spinner />}
+            {!isLoading && (
+                <>
+                    <Toast toastType={toastType} setTypeToast={setToastType} />
+                    <form
+                        className='space-y-6'
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <div className='flex flex-col'>
+                            <Label htmlFor='email' name='Email address' />
+                            <div>
+                                <InputGroup
+                                    id='email'
+                                    type='email'
+                                    name='email'
+                                    placeholder='Email address'
+                                    errors={errors}
+                                    required
+                                    register={register}
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='relative w-full mt-2'>
-                        <InputGroup
-                            type='password'
-                            id='password'
-                            name='password'
-                            placeholder='Password'
-                            errors={errors}
-                            required
-                            register={register}
-                        />
-                    </div>
-                </div>
+                        <div>
+                            <div className='flex items-center justify-between'>
+                                <Label htmlFor='password' name='Password' />
+                                <div className='text-sm'>
+                                    <a
+                                        href='/password/forgot'
+                                        className='font-semibold text-indigo-600 hover:text-indigo-500'
+                                    >
+                                        Forgot password?
+                                    </a>
+                                </div>
+                            </div>
 
-                <div>
-                    <ButtonLogin text={"Login"} type={"submit"} />
-                </div>
-            </form>
+                            <div className='relative w-full mt-2'>
+                                <InputGroup
+                                    type='password'
+                                    id='password'
+                                    name='password'
+                                    placeholder='Password'
+                                    errors={errors}
+                                    required
+                                    register={register}
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <ButtonLogin text={"Login"} type={"submit"} />
+                        </div>
+                    </form>
+                </>
+            )}
         </>
     );
 };
