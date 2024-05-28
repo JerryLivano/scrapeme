0be7460d30391875eadbdee39c175fbabe62c228
JerryLivano <2172002@maranatha.ac.id>
2024-05-28 14:50:16 +0700

    fix: fetch data application

--- src/pages/ManageApp/AddApplication/Index.jsx
+++ src/pages/ManageApp/AddApplication/Index.jsx
@@ -1,7 +1,16 @@
+import Spinner from "../../../components/elements/Spinner/Spinner";
+import { useGetApplicationQuery } from "../../../services/applicationApiSlice";
 import TabApplication from "../TabApplication";
 import FormAddApplication from "./FormAddApplication";
 
 export default function AddApplication() {
+    const {
+        data: applications,
+        isLoading,
+        isSuccess,
+        isError,
+    } = useGetApplicationQuery({ page: 1, limit: 100 });
+
     return (
         <>
             <div className='flex flex-col items-center'>
@@ -9,8 +18,14 @@ export default function AddApplication() {
                     Add Application
                 </div>
             </div>
-            <TabApplication />
-            <FormAddApplication />
+            {isSuccess ? (
+                <>
+                    <TabApplication dataApplications={applications.data} />
+                    <FormAddApplication />
+                </>
+            ) : (
+                <Spinner />
+            )}
         </>
     );
 }
