7ea4baab8532d0062e7d7ba9a1aa39f3491ce672
JerryLivano <2172002@maranatha.ac.id>
2024-05-29 09:55:55 +0700

    fix: UI for add application

--- src/pages/ManageApp/TabApplication.jsx
+++ src/pages/ManageApp/TabApplication.jsx
@@ -5,29 +5,33 @@ export default function TabApplication({
     dataApplications,
     selectedApplication,
 }) {
-
     const [selectedApp, setSelectedApp] = useState(null);
+
     return (
         <>
             <div className='mt-8 mb-10'>
-                <div className='flex items-start font-semibold text-lg py-3'>Application</div>
-                <div className='flex border-b-2 items-center justify-between'>
-                    <div className='flex items-start gap-x-6'>
+                <div className='flex items-start font-semibold text-lg py-3'>
+                    Application
+                </div>
+                <div className='flex border-b-2 py-2 border-gray-300 items-center justify-between'>
+                    <div className='flex items-start mb-2 gap-x-6'>
                         {dataApplications.map((app) => (
                             <button
-                            key={app.id}
-                            className={`border-b border-white inline-flex text-lg pb-5 hover:text-blue-700 hover:border-b-blue-700 h-fit ${
-                              selectedApp === app.id ? "border-b-blue-700 text-blue-700" : ""
-                            }`}
-                            onClick={() => setSelectedApp(app.id)}
-                          >
-                            {app.name}
-                          </button>
+                                key={app.id}
+                                className={`text-gray-400 inline-flex text-lg pb-3 -mb-4 px-1 h-fit border-b-2 ${
+                                    selectedApp === app.id
+                                        ? "border-b-indigo-600 text-indigo-600"
+                                        : "border-transparent hover:border-indigo-600 hover:text-indigo-600"
+                                }`}
+                                onClick={() => setSelectedApp(app.id)}
+                            >
+                                {app.name}
+                            </button>
                         ))}
-                        
                     </div>
-                    {location.pathname !== "/manage-application/add-application" && (
-                        <div className='flex items-end'>
+                    {location.pathname !==
+                        "/applications/add-application" && (
+                        <div className='absolute right-8 flex items-end'>
                             <ButtonPlus
                                 title={"Application"}
                                 onClick={() => {}}
